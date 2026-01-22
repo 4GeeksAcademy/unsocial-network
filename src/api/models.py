81763e4d4d8bbcc9dc2fd942d3c4302c5dfac478
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2026-01-22 02:02:04 +0000

    :art: Refactor models and routes to implement user, post, comment, and follower functionalities

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -10,10 +10,92 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    def __init__(self, email, password, is_active=True):
+        self.email = email
+        self.password = password
+        self.is_active = is_active
+
+    def serialize(self):
+        return {
+            # "id": self.id,
+            "email": self.email,
+            "is_active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+    
+class Post(db.Model):
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(backref="posts")
+
+    # comments - estan siendo creados automaticamente por el backref en Comment
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "user": self.user.serialize(),
+            "comments": [comment.serialize() for comment in self.comments],
+        }
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id
+        }
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str]= mapped_column(String(120))
+    
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post : Mapped["Post"] = relationship(backref="comments")
+
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(backref="comments")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "author":self.author.serialize()
+        }
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_to: Mapped["User"] = relationship(
+        foreign_keys=[user_to_id], backref="followers")
+
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_from: Mapped["User"] = relationship(
+        foreign_keys=[user_from_id], backref="followings")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_to_id": self.user_to_id,
+            "user_to": self.user_to.serialize(),
+            "user_from_id":self.user_from_id,
+            "user_from": self.user_from.serialize()
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,9 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from flask import request, jsonify, Blueprint
+from api.models import db, User, Post, Comment
+from api.utils import APIException
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -12,11 +12,133 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    users = User.query.all()  # consulta la lista de usuarios en la DB
 
-    return jsonify(response_body), 200
+    list_of_json_users = [user.serialize() for user in users]
+
+    return jsonify(list_of_json_users), 200
+
+
+def reqVal(body, keys):
+    vals = []
+    for key in keys:
+        if key not in body:
+            raise APIException(
+                f'You need to specify the {key}', status_code=400)
+        vals.append(body[key])
+    return vals
+
+
+@api.route('/user', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    keys = ['email', 'password', 'is_active']
+    email, password, is_active = reqVal(body, keys)
+
+    try:
+        new_user = User(email=email, password=password,
+                        is_active=bool(is_active))
+        new_user.save()
+
+    except Exception as e:
+        raise APIException('Error creating the user: ' +
+                           str(e), status_code=500)
+
+    return jsonify(new_user.serialize()), 201
+
+
+@api.route('/user/<string:email>')
+def get_user_by_email(email):
+
+    wanted = User.get_by_email(email)
+
+    if wanted is None:
+        raise APIException('User not found', status_code=404)
+
+    return jsonify(wanted.serialize()), 200
+
+
+@api.route('/user/<string:email>', methods=['PUT', 'DELETE'])
+def update_user_by(email):
+
+    edited = User.get_by_email(email)
+    if edited is None:
+        raise APIException('User not found', status_code=404)
+
+    if request.method == 'DELETE':
+        try:
+            db.session.delete(edited)
+            db.session.commit()
+
+        except Exception as e:
+            raise APIException('Error deleting the user: ' +
+                               str(e), status_code=500)
+
+        return jsonify({"msg": "User deleted"}), 200
+
+    body = request.get_json()
+    if 'is_active' not in body:
+        raise APIException(
+            'You need to specify the is_active field', status_code=400)
+
+    if 'is_active' in body:
+        try:
+            edited.is_active = bool(body['is_active'])
+            db.session.commit()
+        except Exception as e:
+            raise APIException('Error updating the user: ' +
+                               str(e), status_code=500)
+
+    return jsonify(edited.serialize()), 200
+
+
+@api.route('/post/<int:user_id>', methods=['POST'])
+def create_post(user_id):
+    body = request.get_json()
+
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+
+    post = Post()
+    post.user_id = user_id
+
+    db.session.add(post)
+    db.session.commit()
+
+    return jsonify(post.serialize()), 201
+
+
+@api.route('/post', methods=['GET'])
+def get_posts():
+    posts = Post.query.all()
+    list_of_json_posts = [post.serialize() for post in posts]
+
+    return jsonify(list_of_json_posts), 200
+
+
+@api.route('/comment/<int:post_id>', methods=['POST'])
+def create_comment(post_id):
+    body = request.get_json()
+    keys = ['comment_text', 'author_id']
+    comment_text, author_id = reqVal(body, keys)
+
+    post = Post.query.get(post_id)
+    if post is None:
+        raise APIException('Post not found', status_code=404)
+
+    author = User.query.get(author_id)
+    if author is None:
+        raise APIException('Author not found', status_code=404)
+
+    comment = Comment()
+    comment.comment_text = comment_text
+    comment.post_id = post_id
+    comment.author_id = author_id
+    db.session.add(comment)
+    db.session.commit()
+
+    return jsonify(comment.serialize()), 201
